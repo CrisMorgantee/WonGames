3e1af2f905a0c29862814408f891cef694633941
Cristiano <cristianomorgante@hotmail.com>
2020-09-07 16:16:41 -0300

    Inicial commit and Add category collection

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ffa83edb276ea8cd8f33bb250f36dcab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5009baf0-a268-419a-8c03-e1bc12ac37eb'
+};
