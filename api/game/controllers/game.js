e666c0dea9d00e29c0a350c2efba7e69032f5b48
Cristiano <Morgante>
2020-10-01 17:40:44 -0300

    Create populate games structure

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,13 +1,18 @@
 "use strict";
 
+const { default: createStrapi } = require("strapi");
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
 module.exports = {
-  populate: (ctx) => {
-    console.log("Initialize");
-    ctx.send({ ok: true });
+  populate: async (ctx) => {
+    console.log("Starting populate...");
+
+    await strapi.services.game.populate();
+
+    ctx.send("Finishing populating!");
   },
 };
